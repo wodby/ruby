a99f451ab3a55a6aa13c1a5061221e2f5c06d06e
Chingis S <chingis@chingis.io>
2026-09-23 09:42:17 +0400

    Respect runtime-specific test image inputs
    The current make test target exports RUBY_IMAGE. Require that value when launching the tool smoke check and report a clear error when the harness input is missing.

--- tests/run.sh
+++ tests/run.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Validate the development tool contract before application integration tests.
-docker run --rm --network none --entrypoint /bin/sh -v "$PWD/development-tools.sh:/tmp/development-tools.sh:ro" "${IMAGE}" /tmp/development-tools.sh
+docker run --rm --network none --entrypoint /bin/sh -v "$PWD/development-tools.sh:/tmp/development-tools.sh:ro" "${RUBY_IMAGE:?must be supplied by make test}" /tmp/development-tools.sh
 
 if [[ -n "${DEBUG}" ]]; then
     set -x
